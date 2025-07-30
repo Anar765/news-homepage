dbe0f55b6d147274894d6429c300a27c82ad730b
Anar765 <anar.aliyev181@gmail.com>
2025-07-30 21:27:50 +0400

    Add Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Header from './components/Header';
 import Main from './components/Main';
+import { Analytics } from '@vercel/analytics/react';
 
 const App = () => {
   return (
     <>
       <Header />
       <Main />
+      <Analytics />
     </>
   )
 }
